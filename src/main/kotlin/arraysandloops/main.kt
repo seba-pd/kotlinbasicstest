a8cf4baa2b0b4842d2e45d67be060471be9926c8
bartnicki <lukasz.bartnicki.external@easy-software.com>
2022-03-20 11:55:40 +0100

    arrays and loops 2

--- src/main/kotlin/arraysandloops/main.kt
+++ src/main/kotlin/arraysandloops/main.kt
@@ -1,6 +1,11 @@
 package arraysandloops
 
-/* Ta funkcja dla podanej jliczby całkowitej  */
+/*
+     a % b należy do zbioru <0, b-1>
+     7 = 2*3 + 1  //1 - reszta z dzielenia 7 przez 3 i przez 2
+ */
+
+/* Ta funkcja dla podanej liczby całkowitej zwraca ilość zer na końcu liczby */
 // f(100) -> n = 1 k=2
 fun numOfZero(arg : Int) : Int {
     var k = 0
@@ -12,8 +17,27 @@ fun numOfZero(arg : Int) : Int {
     return k
 }
 
+fun digitSum(arg : Int) : Int {
+    var sum = 0
+    var n = Math.abs(arg)
+    while(n > 0) {
+        sum += n % 10
+        n /= 10
+    }
+    return sum
+}
+
+fun prepareSequence(n : Int) : Array<String?> {
+    val array = arrayOfNulls<String>(n) //to nie jest immutable
+    for(i in 0 .. n-1) {
+        array[i] = "word" + i
+    }
+    return array
+}
+
 fun main(args : Array<String>) {
 
+    //funkcje można zagnieżdżać
   /*  fun f() {
         fun g() {
 
@@ -34,4 +58,22 @@ fun main(args : Array<String>) {
     println()
     println(numOfZero(3221000));
     println(numOfZero(101));
+
+    println(digitSum(-123))
+    println(digitSum(10201))
+    println(digitSum(7))
+
+    val array1 = arrayOf(12, 31, 14, 51, 16, 71) //arrayOf zwraca nam immutable array (nie mozna dodac wartosci)
+
+    var sum = 0
+    for(v in array1) {
+        sum += v
+    }
+
+    println(sum)
+
+    //println(prepareSequence(15))
+    for(v in prepareSequence(15)) {
+        print(v + " ")
+    }
 }
